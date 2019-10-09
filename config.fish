e86f9681be1ab25a4f271b2b0a372519dc568316
metaporia <kyahnkrafft@me.com>
2019-10-08 20:41:49 -0700

    fish: stack: add ghc 8.6.5, res. 14.3 bin/ to PATH

--- config.fish
+++ config.fish
@@ -10,7 +10,9 @@ set -gx PATH /home/aporia/.cargo/bin \
              /home/aporia/scripts \
              /usr/bin \
              /usr/local/bin \
-             /home/aporia/.nvm/versions/node/v11.12.0/bin/ 
+             /home/aporia/.stack/programs/x86_64-linux/ghc-tinfo6-8.6.5/bin \
+             /home/aporia/.nvm/versions/node/v11.12.0/bin
+
             #/opt/anaconda/bin/
             #/home/aporia/.stack/programs/x86_64-linux/ghcjs-0.2.0.9006020_ghc-7.10.3/bin
             #/home/aporia/.conda3/bin \
