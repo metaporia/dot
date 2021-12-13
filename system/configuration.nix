4974d8ce51e2d0d5284d49ad0e50e81c882d6902
metaporia <kyahnkrafft@me.com>
2021-12-13 12:24:54 -0800

    nixos: set timezone to LA

--- system/configuration.nix
+++ system/configuration.nix
@@ -50,7 +50,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  #time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Los_Angeles";
   services.localtime.enable = true;
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
