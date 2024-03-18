8abd36910aa7fda98c194617765690c4465a89f8
metaporia <kyahnkrafft@me.com>
2024-03-18 10:43:49 -0700

    Revert NIX_PATH change to test

--- overlays-compat/overlays.nix
+++ overlays-compat/overlays.nix
@@ -2,7 +2,7 @@ final: prev:
 with prev.lib;
 let
   # Load the system config and get the `nixpkgs.overlays` option
-  overlays = (import <nixpkgs/nixos> { }).config.nixpkgs.overlays;
+  overlays = (import <nixpkgs> { }).config.nixpkgs.overlays;
 in
   # Apply all overlays to the input of the current "main" overlay
   foldl' (flip extends) (_: prev) overlays final
