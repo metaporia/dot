5b3bf1347640378f19058b083117f0cf042ecaf7
metaporia <kyahnkrafft@me.com>
2024-03-25 10:03:48 -0700

    nvim: fix telescope-fzf find_files keybind

--- config/nvim/lua/aporia/plugins/init.lua
+++ config/nvim/lua/aporia/plugins/init.lua
@@ -101,7 +101,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>b', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
