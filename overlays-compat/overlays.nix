4c9992d9f61ae800dd353b79ed150ad5023541c9
metaporia <kyahnkrafft@me.com>
2024-09-22 12:09:25 -0700

    Remove overlay-compat/

--- overlays-compat/overlays.nix
+++
@@ -1,8 +0,0 @@
-final: prev:
-with prev.lib;
-let
-  # Load the system config and get the `nixpkgs.overlays` option
-  overlays = (import <nixpkgs> { }).config.nixpkgs.overlays;
-in
-  # Apply all overlays to the input of the current "main" overlay
-  foldl' (flip extends) (_: prev) overlays final
